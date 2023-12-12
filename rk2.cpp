059f77091b68440822a70cf85b56460652523a43
Evan <54749606+madC0BR4@users.noreply.github.com>
2023-12-12 14:51:42 +0300

    Add files via upload

---
+++ rk2.cpp
@@ -0,0 +1,91 @@
+//Вариант 5
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class MusicalInstrument {
+    protected:
+        string name;
+        string material;
+    public:
+        MusicalInstrument(string i_name, string i_material) {
+            name = i_name;
+            material = i_material;
+        }
+
+        void setName(const string& n) {
+            name = n;
+        }
+
+        void setMaterial(const string& m) {
+            material = m;
+        }
+
+        virtual void display() const{
+            cout << "   Name of instrument: " <<name <<endl;
+            cout << "   Material: " << material <<endl;
+        }
+
+        ~MusicalInstrument(){}; 
+};
+
+class StringInstrument: public MusicalInstrument {
+    protected:
+        int strings;
+    public:
+        StringInstrument(const string& n, const string& m, const int& s): MusicalInstrument(n,m), strings(s) {};
+
+        void display() const override {
+            cout << "   Name of instrument: " <<name <<endl;
+            cout << "   Material: " << material <<endl;
+            cout << "   Strings: " << strings << endl;
+        }
+
+        ~StringInstrument(){};
+};
+
+class BrassInstrument: public MusicalInstrument {
+    protected:
+        string brass_type;
+    public:
+        BrassInstrument(const string& n, const string& m, const string& b): MusicalInstrument(n,m), brass_type(b) {};
+
+        void display() const override {
+            cout << "   Name of instrument: " <<name <<endl;
+            cout << "   Material: " << material <<endl;
+            cout << "   Type of brass: " << brass_type << endl;
+        }
+
+        ~BrassInstrument(){};
+};
+
+void UpdateObj(MusicalInstrument* ptr) {
+    ptr->setMaterial("maple");
+    ptr->setName("bass");
+}
+
+int main() {
+    vector<MusicalInstrument*> vec;
+    vec.resize(3);
+    vec[0] = new MusicalInstrument("Drums", "Metal");
+    vec[1] = new StringInstrument("Guitar", "rosewood", 6);
+    vec[2] = new BrassInstrument("Saxophone", "brass", "strong");
+
+    for (const auto i: vec) {
+        cout << "Instrument: " << endl;
+        i->display();
+        cout << "----------------------" << endl;
+    }
+    
+    for (const auto i: vec) {
+        cout << "Instrument: " << endl;
+        UpdateObj(i);
+        i->display();
+        cout << "----------------------" << endl;
+        delete i;
+    }
+
+    return 0;
+}
